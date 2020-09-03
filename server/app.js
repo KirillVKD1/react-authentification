4998b5fd78b1deb425bcac2efcad9ba2a5751331
Kirill Veretelnikov <k.veretelnikov.exceedteam@gmail.com>
2020-09-03 17:24:39 +0300

    add-cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const config = require("config");
+const config = require("config"); 
+const cors = require('cors')
 
 const PORT = config.get('port') || 3010;
-
 
 const app = express();
 const product = require('./routes/product.route');
@@ -13,7 +13,6 @@ app.use(express.json({ extended: true }));//body output
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 // Connect to MongoDB 
 async function start() {
@@ -30,20 +29,9 @@ async function start() {
 }
 start();
 
-// Enable CORS
-app.use((req, res, next) => {
-  //to allow cross domain requests to send cookie information.
-  res.header('Access-Control-Allow-Credentials', true);
-  // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  // list of methods that are supported by the server
-  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers" ,"Origin, X-Requested-With, Content-Type, Accept, Authorization"); 
-  next();
-});
-
+// Enable CORS 
+app.use(cors())
 
 app.use(product);
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
